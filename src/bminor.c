66b2f760d177f5c844e877b3d2594de376bc5509
Eamon Tracey <etracey@student05.cse.nd.edu>
2023-08-30 15:04:54 -0400

    remove support for multiple input files

--- src/bminor.c
+++ src/bminor.c
@@ -7,24 +7,17 @@
 int encode_file(const char *path);
 
 int main(int argc, char *argv[]) {
-    int exc = EXIT_SUCCESS;
-
-    if (argc < 3) {
-        fprintf(stderr, "error: invalid arguments, expecting option and filenames.\n");
+    if (argc != 3) {
+        fprintf(stderr, "error: invalid arguments, expecting option and filename.\n");
         return EXIT_FAILURE;
     }
 
-    if (!strcmp(argv[1], "--encode")) {
-        for (int i = 2; i < argc; ++i) {
-            if (encode_file(argv[i]) == -1)
-                exc = EXIT_FAILURE;
-        }
-    } else {
+    if (!strcmp(argv[1], "--encode"))
+        return encode_file(argv[2]) == -1 ? EXIT_FAILURE : EXIT_SUCCESS;
+    else {
         fprintf(stderr, "error: unknown option \"%s\".\n", argv[1]);
         return EXIT_FAILURE;
     }
-
-    return exc;
 }
 
 int encode_file(const char *path) {
