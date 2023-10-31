aa1d32f85e4b47600588e8b6d1840cffb5594ef7
Eamon Tracey <etracey@student05.cse.nd.edu>
2023-10-30 23:27:05 -0400

    fix nasty associativity bugs

--- src/expr.c
+++ src/expr.c
@@ -86,104 +86,106 @@ void expr_print(struct expr *e, int paren) {
      * Note: Parentheticals also necessary for some same-precedence,
      * non-associative operators (e.g., 1 - (2 + 3)).
      */
-    if (paren > 0)
+    if (paren)
         fprintf(stdout, "(");
 
     switch (e->kind) {
     case EXPR_INC:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "++");
         break;
     case EXPR_DEC:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "--");
         break;
     case EXPR_NOT:
         fprintf(stdout, "!");
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0 || e->left->kind == EXPR_NOT);
         break;
     case EXPR_EXP:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "^");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0);
         break;
     case EXPR_MULT:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "*");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0);
         break;
     case EXPR_DIV:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "/");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0 || e->right->kind == EXPR_DIV);
         break;
     case EXPR_MOD:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "%%");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0);
         break;
     case EXPR_PLUS:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "+");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        if (e->right->kind == EXPR_POS) fprintf(stdout, " ");
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0);
         break;
     case EXPR_MINUS:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "-");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        if (e->right->kind == EXPR_NEG) fprintf(stdout, " ");
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0 || e->right->kind == EXPR_PLUS);
         break;
     case EXPR_LT:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "<");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0);
         break;
     case EXPR_LTE:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "<=");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0);
         break;
     case EXPR_GT:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, ">");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0);
         break;
     case EXPR_GTE:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, ">=");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0);
         break;
     case EXPR_EQ:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "==");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0);
         break;
     case EXPR_NOTEQ:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "!=");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0);
         break;
     case EXPR_AND:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "&&");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0);
         break;
     case EXPR_OR:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "||");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0);
         break;
     case EXPR_ASSIGN:
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         fprintf(stdout, "=");
-        expr_print(e->right, precdif(e->kind, e->right->kind));
+        expr_print(e->right, precdif(e->kind, e->right->kind) > 0);
         break;
     case EXPR_POS:
         fprintf(stdout, "+");
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         break;
     case EXPR_NEG:
         fprintf(stdout, "-");
-        expr_print(e->left, precdif(e->kind, e->left->kind));
+        expr_print(e->left, precdif(e->kind, e->left->kind) > 0);
         break;
     case EXPR_ARRACC:
         expr_print(e->left, 0);
@@ -238,6 +240,23 @@ int precedences[] = {
     7, 7, 9, 9, 9, 9, 9, 9, 9,
     9, 9, -1
 };
+
+/*
+ * These tables shall remain a WIP for now.
+int left_assoc[] = {
+    0, 0, 0, 0, 1, 0, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 0, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1
+};
+
+int right_assoc[] = {
+    0, 0, 0, 1, 1, 0, 1, 1,
+    0, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 0, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1
+};
+*/
 
 int precdif(expr_t kind1, expr_t kind2) {
     return precedences[kind1] - precedences[kind2];
