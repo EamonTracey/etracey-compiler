024c193b819fcfa98214dabf848bcf7948c49ae1
Eamon Tracey <etracey@student05.cse.nd.edu>
2023-10-30 22:28:44 -0400

    add more test cases

--- test/test_printer.sh
+++ test/test_printer.sh
@@ -5,14 +5,15 @@ RED="\e[31m"
 NOCOLOR="\e[0m"
 
 # For the printer, there are no 'bad' test cases.
-# Rather, ensure printing and reprinting yield
-# the exact same output.
+# Rather, ensure printing and reprinting succeed 
+# and yield the exact same output.
 echo "Testing good printer cases..."
 for testfile in $(ls test/printer/good*.bminor)
 do
-    ./bminor --print $testfile > $testfile.out1 2>&1
-    ./bminor --print $testfile.out1 > $testfile.out2 2>&1
-	if cmp $testfile.out1 $testfile.out2
+	if
+        ./bminor --print $testfile > $testfile.out1 2>&1 && 
+        ./bminor --print $testfile.out1 > $testfile.out2 2>&1 &&
+        cmp $testfile.out1 $testfile.out2
 	then
         echo -e "${GREEN}PASS${NOCOLOR}: $testfile"
 	else
