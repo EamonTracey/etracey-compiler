53a6bfc3ab8910979a5c7efe8aa3b4f0565f0495
Eamon Tracey <etracey@student05.cse.nd.edu>
2023-11-05 14:56:33 -0500

    implement symbol_create

--- src/symbol.c
+++ src/symbol.c
@@ -3,5 +3,15 @@
 #include "symbol.h"
 
 struct symbol* symbol_create(symbol_t kind, struct type *type, char *name) {
-    return NULL;
+    struct symbol *symbol = (struct symbol *)malloc(sizeof(struct symbol));
+
+    if (symbol == NULL)
+        return NULL;
+
+    symbol->kind = kind;
+    symbol->type = type;
+    symbol->name = name;
+    symbol->which = 0;
+
+    return symbol;
 }
